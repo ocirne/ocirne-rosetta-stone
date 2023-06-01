b94bd840d5701be6e41d41799f7b0a6fdbcb0e9d
Enrico Pilz <kontakt@enricopilz.de>
2023-06-02 00:15:53 +0200

    Minor

--- kotlin/src/main/kotlin/io/github/ocirne/rosetta/projecteuler/Problem8.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/rosetta/projecteuler/Problem8.kt
@@ -3,12 +3,12 @@ package io.github.ocirne.rosetta.projecteuler
 class Problem8 {
 
     fun solve(length: Int): Long {
-        return (0..content.length - length).map { i ->
-            content.substring(i, i+length)
+        return (0..content.length - length).maxOf { i ->
+            content.substring(i, i + length)
                 .toList()
                 .map { c -> c.digitToInt().toLong() }
                 .reduce { a, b -> a * b }
-        }.max()
+        }
     }
     companion object {
         val content = this::class.java.classLoader
