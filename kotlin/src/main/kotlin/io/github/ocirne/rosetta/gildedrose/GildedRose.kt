9663eb8b87a395d865d98db89ef0ce33e312a564
Enrico Pilz <kontakt@enricopilz.de>
2023-07-06 23:24:09 +0200

    Extract in methods

--- kotlin/src/main/kotlin/io/github/ocirne/rosetta/gildedrose/GildedRose.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/rosetta/gildedrose/GildedRose.kt
@@ -8,51 +8,76 @@ class GildedRose(var items: List<Item>) {
                 handleLegendaryItem(item)
                 continue
             }
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
+            if (item.name == "Aged Brie") {
+                handleAgedBrie(item)
+                continue
             }
-
-            item.sellIn = item.sellIn - 1
-
-            if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1
-                        }
-                    } else {
-                        item.quality = 0
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
-                }
+            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                handleBackstagePass(item)
+                continue
             }
+            handleDefault(item)
         }
     }
 
     fun handleLegendaryItem(item: Item) {
         // do nothing
+    }
+
+    fun handleAgedBrie(item: Item) {
+        if (item.name != "Aged Brie") {
+            return
+        }
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+
+        }
+        item.sellIn = item.sellIn - 1
+        if (item.sellIn < 0) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+            }
+        }
+    }
+
+    fun handleBackstagePass(item: Item) {
+        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            return
+        }
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+        }
+
+        item.sellIn = item.sellIn - 1
+
+        if (item.sellIn < 0) {
+            item.quality = 0
+        }
+    }
+
+    fun handleDefault(item: Item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1
+        }
+
+        item.sellIn = item.sellIn - 1
+
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1
+            }
+        }
     }
 }
