4c112b18096354bab0543c8a9f10eb113fa87a92
Enrico Pilz <kontakt@enricopilz.de>
2023-07-07 23:35:21 +0200

    Different classes

--- kotlin/src/main/kotlin/io/github/ocirne/rosetta/gildedrose/GildedRose.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/rosetta/gildedrose/GildedRose.kt
@@ -3,81 +3,82 @@ package io.github.ocirne.rosetta.gildedrose
 class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
-        for (item in items) {
-            if (item.name == "Sulfuras, Hand of Ragnaros") {
-                handleLegendaryItem(item)
-                continue
-            }
-            if (item.name == "Aged Brie") {
-                handleAgedBrie(item)
-                continue
-            }
-            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                handleBackstagePass(item)
-                continue
-            }
-            handleDefault(item)
-        }
+        items.forEach { updateQuality(it) }
     }
 
-    fun handleLegendaryItem(item: Item) {
-        // do nothing
+    fun updateQuality(item: Item) {
+        when (item.name) {
+            "Sulfuras, Hand of Ragnaros" -> LegendaryItem(item)
+            "Aged Brie" -> CheeseItem(item)
+            "Backstage passes to a TAFKAL80ETC concert" -> BackstagePassItem(item)
+            else -> OrdinaryItem(item)
+        }.age()
     }
 
-    fun handleAgedBrie(item: Item) {
-        if (item.name != "Aged Brie") {
-            return
-        }
-        if (item.quality < 50) {
-            item.quality = item.quality + 1
-
-        }
-        item.sellIn = item.sellIn - 1
-        if (item.sellIn < 0) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-            }
-        }
+    abstract class AgingItem(val item: Item) {
+        abstract fun age()
     }
 
-    fun handleBackstagePass(item: Item) {
-        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            return
-        }
-        if (item.quality < 50) {
-            item.quality = item.quality + 1
-
-            if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
-            }
-
-            if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
-            }
-        }
-
-        item.sellIn = item.sellIn - 1
-
-        if (item.sellIn < 0) {
-            item.quality = 0
-        }
-    }
-
-    fun handleDefault(item: Item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1
-        }
-
-        item.sellIn = item.sellIn - 1
-
-        if (item.sellIn < 0) {
+    class OrdinaryItem(item: Item): AgingItem(item) {
+        override fun age() {
             if (item.quality > 0) {
                 item.quality = item.quality - 1
             }
+
+            item.sellIn = item.sellIn - 1
+
+            if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1
+                }
+            }
+        }
+    }
+
+    class CheeseItem(item: Item): AgingItem(item) {
+        override fun age() {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+
+            }
+            item.sellIn = item.sellIn - 1
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+        }
+    }
+
+    class BackstagePassItem(item: Item): AgingItem(item) {
+        override fun age() {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+            }
+
+            item.sellIn = item.sellIn - 1
+
+            if (item.sellIn < 0) {
+                item.quality = 0
+            }
+        }
+    }
+
+    class LegendaryItem(item: Item): AgingItem(item) {
+        override fun age() {
+            // do nothing
         }
     }
 }
